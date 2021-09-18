5c547ac11a303094c09f24d7556932f0742f5dee
Roi Godelman <roigodelman@abigails-mbp.myfiosgateway.com>
2021-09-18 07:26:41 -0400

    first

---
+++ src/main/java/com/example/javatemplate/JavaTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.example.javatemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaTemplateApplication.class, args);
+	}
+
+}
