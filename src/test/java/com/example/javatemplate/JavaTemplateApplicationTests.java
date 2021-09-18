04245f80502a0c6c05b2e6d9b117f8ff4b722eb8
Roi Godelman <roigodelman@abigails-mbp.myfiosgateway.com>
2021-09-18 11:25:12 -0400

    break test

--- src/test/java/com/example/javatemplate/JavaTemplateApplicationTests.java
+++ src/test/java/com/example/javatemplate/JavaTemplateApplicationTests.java
@@ -2,12 +2,16 @@ package com.example.javatemplate;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class JavaTemplateApplicationTests {
 
 	@Test
 	void contextLoads() {
+
+		Assert.isTrue(false);
+
 	}
 
 }
